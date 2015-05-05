2f3ca52d2ac7f1ccf963b76ec6e49d0044d5d910
Ismael Jimenez <ijimenezgarzon@gmail.com>
2015-05-04 23:46:10 -0600

    Added actions to be executed with the buttons

--- app/src/main/java/co/ismapro/android_actionbar/MainActivity.java
+++ app/src/main/java/co/ismapro/android_actionbar/MainActivity.java
@@ -3,7 +3,9 @@ package co.ismapro.android_actionbar;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 // when the app extends from ActionBarActivity it shows the action bar
 // in Android versions previous 3.0, using the Android Support Library.
@@ -19,22 +21,38 @@ public class MainActivity extends ActionBarActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        // Handle presses on the action bar items
+        switch (item.getItemId()) {
+            case R.id.action_search:
+                openSearch();
+                return true;
+            case R.id.action_settings:
+                openSettings();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
         }
-
-        return super.onOptionsItemSelected(item);
     }
+
+    // Method to manage the search button action
+    public void openSearch() {
+        // get the TextView and set the Search text
+        TextView appText = (TextView)findViewById(R.id.appText);
+        appText.setText("Search Action executed!");
+    }
+
+    // Method to manage the settings action
+    public void openSettings() {
+        // get the TextView and set the Settings text
+        TextView appText = (TextView)findViewById(R.id.appText);
+        appText.setText("Settings Action executed!");
+    }
+
 }
